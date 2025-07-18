20d5d9a27b5db24d19298e1376326b4203a267d9
valentin-iordanov <72465027+valentin-iordanov@users.noreply.github.com>
2025-07-18 21:00:52 +0300

    Fix Next.js build

--- frontend/src/app/components/Location.tsx
+++ frontend/src/app/components/Location.tsx
@@ -12,14 +12,15 @@ export default function Location() {
     <section className={styles.location}>
       <h2>Локация</h2>
       <iframe
+        className={styles.mapIframe}
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2907.926995412542!2d24.92300731545017!3d42.819525379159624!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0:0x0!2zNDLCsDQ5JzA5LjMiTiAyNMKwNTUnMjAuNCJF!5e0!3m2!1sen!2sbg!4v1715281213123"
         width="600"
         height="450"
-        allowFullScreen=""
+        allowFullScreen
         loading="lazy"
         referrerPolicy="no-referrer-when-downgrade"
       ></iframe>
-      <button onClick={openMap}>Отвори в Google Maps</button>
+      <button className={styles.mapButton} onClick={openMap}>Отвори в Google Maps</button>
     </section>
   );
 }
